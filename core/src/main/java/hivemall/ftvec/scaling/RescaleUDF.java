bcae1534a2894d98746210ed20985c115a99678e
Yuming Wang <wgyumg@gmail.com>
2017-02-16 15:16:45 +0900

    Close #45: [HIVEMALL-71] Handle null values and add a unit Tests to RescaleUDF

--- core/src/main/java/hivemall/ftvec/scaling/RescaleUDF.java
+++ core/src/main/java/hivemall/ftvec/scaling/RescaleUDF.java
@@ -20,8 +20,12 @@ package hivemall.ftvec.scaling;
 
 import static hivemall.utils.hadoop.WritableUtils.val;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
+
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.UDFType;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
@@ -36,49 +40,82 @@ import org.apache.hadoop.io.Text;
 @UDFType(deterministic = true, stateful = false)
 public final class RescaleUDF extends UDF {
 
-    public FloatWritable evaluate(final float value, final float min, final float max) {
+    @Nullable
+    public FloatWritable evaluate(@Nullable final Double value, @CheckForNull final Double min,
+            @CheckForNull final Double max) throws HiveException {
+        return evaluate(double2Float(value), double2Float(min), double2Float(max));
+    }
+
+    @Nullable
+    public FloatWritable evaluate(@Nullable final Float value, @CheckForNull final Float min,
+            @CheckForNull final Float max) throws HiveException {
+        if (value == null) {
+            return null;
+        }
+
+        if (min == null)
+            throw new HiveException("min should not be null");
+        if (max == null)
+            throw new HiveException("max should not be null");
+
         return val(min_max_normalization(value, min, max));
     }
 
-    public FloatWritable evaluate(final double value, final double min, final double max) {
-        return val(min_max_normalization(value, min, max));
+    @Nullable
+    public Text evaluate(@Nullable final String s, @CheckForNull final Double min,
+            @CheckForNull final Double max) throws HiveException {
+        return evaluate(s, double2Float(min), double2Float(max));
     }
 
-    public Text evaluate(final String s, final double min, final double max) {
-        String[] fv = s.split(":");
-        if (fv.length != 2) {
-            throw new IllegalArgumentException("Invalid feature value representation: " + s);
+    @Nullable
+    public Text evaluate(@Nullable final String s, @CheckForNull final Float min,
+            @CheckForNull final Float max) throws HiveException {
+        if (s == null) {
+            return null;
         }
-        double v = Float.parseFloat(fv[1]);
-        float scaled_v = min_max_normalization(v, min, max);
-        String ret = fv[0] + ':' + Float.toString(scaled_v);
+
+        if (min == null)
+            throw new HiveException("min should not be null");
+        if (max == null)
+            throw new HiveException("max should not be null");
+
+        final String[] fv = s.split(":");
+        if (fv.length != 2) {
+            throw new HiveException(String.format("Invalid feature value " + "representation: %s",
+                s));
+        }
+        float v;
+        try {
+            v = Float.parseFloat(fv[1]);
+        } catch (NumberFormatException e) {
+            throw new HiveException(String.format("Invalid feature value "
+                    + "representation: %s, %s can't parse to float.", s, fv[1]));
+        }
+
+        float scaled_v = min_max_normalization(v, min.floatValue(), max.floatValue());
+        String ret = fv[0] + ':' + scaled_v;
         return val(ret);
     }
 
-    public Text evaluate(final String s, final float min, final float max) {
-        String[] fv = s.split(":");
-        if (fv.length != 2) {
-            throw new IllegalArgumentException("Invalid feature value representation: " + s);
+    private static float min_max_normalization(final float value, final float min, final float max)
+            throws HiveException {
+        if (min > max) {
+            throw new HiveException("min value `" + min + "` SHOULD be less than max value `" + max
+                    + '`');
         }
-        float v = Float.parseFloat(fv[1]);
-        float scaled_v = min_max_normalization(v, min, max);
-        String ret = fv[0] + ':' + Float.toString(scaled_v);
-        return val(ret);
-    }
-
-    private static float min_max_normalization(final float value, final float min, final float max) {
         if (min == max) {
             return 0.5f;
         }
         return (value - min) / (max - min);
     }
 
-    private static float min_max_normalization(final double value, final double min,
-            final double max) {
-        if (min == max) {
-            return 0.5f;
+    @Nullable
+    private static Float double2Float(@Nullable final Double value) {
+        if (value == null) {
+            return null;
+        } else {
+            return Float.valueOf(value.floatValue());
         }
-        return (float) ((value - min) / (max - min));
     }
 
 }
