ffee4ef90c54b23ee0a1d78683ff882ac290ff24
myui <yuin405@gmail.com>
2017-02-23 18:31:15 +0900

    Added unit tests for DenseMatrixBuilder

--- core/src/test/java/hivemall/matrix/MatrixBuilderTest.java
+++ core/src/test/java/hivemall/matrix/MatrixBuilderTest.java
@@ -147,6 +147,40 @@ public class MatrixBuilderTest {
     }
 
     @Test
+    public void testReadOnlyDenseMatrix2dSparseInput() {
+        Matrix matrix = denseMatrixSparseInput();
+        Assert.assertEquals(6, matrix.numRows());
+        Assert.assertEquals(6, matrix.numColumns());
+        Assert.assertEquals(4, matrix.numColumns(0));
+        Assert.assertEquals(3, matrix.numColumns(1));
+        Assert.assertEquals(6, matrix.numColumns(2));
+        Assert.assertEquals(5, matrix.numColumns(3));
+        Assert.assertEquals(6, matrix.numColumns(4));
+        Assert.assertEquals(6, matrix.numColumns(5));
+
+        Assert.assertEquals(11d, matrix.get(0, 0), 0.d);
+        Assert.assertEquals(12d, matrix.get(0, 1), 0.d);
+        Assert.assertEquals(13d, matrix.get(0, 2), 0.d);
+        Assert.assertEquals(14d, matrix.get(0, 3), 0.d);
+        Assert.assertEquals(22d, matrix.get(1, 1), 0.d);
+        Assert.assertEquals(23d, matrix.get(1, 2), 0.d);
+        Assert.assertEquals(33d, matrix.get(2, 2), 0.d);
+        Assert.assertEquals(34d, matrix.get(2, 3), 0.d);
+        Assert.assertEquals(35d, matrix.get(2, 4), 0.d);
+        Assert.assertEquals(36d, matrix.get(2, 5), 0.d);
+        Assert.assertEquals(44d, matrix.get(3, 3), 0.d);
+        Assert.assertEquals(45d, matrix.get(3, 4), 0.d);
+        Assert.assertEquals(56d, matrix.get(4, 5), 0.d);
+        Assert.assertEquals(66d, matrix.get(5, 5), 0.d);
+
+        Assert.assertEquals(0.d, matrix.get(5, 4), 0.d);
+
+        Assert.assertEquals(0.d, matrix.get(1, 0), 0.d);
+        Assert.assertEquals(0.d, matrix.get(1, 3), 0.d);
+        Assert.assertEquals(0.d, matrix.get(1, 0), 0.d);
+    }
+
+    @Test
     public void testReadOnlyDenseMatrix2dFromLibSVM() {
         Matrix matrix = denseMatrixFromLibSVM();
         Assert.assertEquals(6, matrix.numRows());
@@ -259,6 +293,25 @@ public class MatrixBuilderTest {
         builder.nextRow(new double[] {0, 0, 0, 44, 45});
         builder.nextRow(new double[] {0, 0, 0, 0, 0, 56});
         builder.nextRow(new double[] {0, 0, 0, 0, 0, 66});
+        return builder.buildMatrix(true);
+    }
+
+    private static Matrix denseMatrixSparseInput() {
+        /*
+        11  12  13  14  0   0
+        0   22  23  0   0   0
+        0   0   33  34  35  36
+        0   0   0   44  45  0
+        0   0   0   0   0   56
+        0   0   0   0   0   66
+        */
+        DenseMatrixBuilder builder = new DenseMatrixBuilder(1024);
+        builder.nextColumn(0, 11).nextColumn(1, 12).nextColumn(2, 13).nextColumn(3, 14).nextRow();
+        builder.nextColumn(1, 22).nextColumn(2, 23).nextRow();
+        builder.nextColumn(2, 33).nextColumn(3, 34).nextColumn(4, 35).nextColumn(5, 36).nextRow();
+        builder.nextColumn(3, 44).nextColumn(4, 45).nextRow();
+        builder.nextColumn(5, 56).nextRow();
+        builder.nextColumn(5, 66).nextRow();
         return builder.buildMatrix(true);
     }
 
