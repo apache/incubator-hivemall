ede6c9d4922005ad9b12efcce4837cf66f09ec79
amaya <mail@sapphire.in.net>
2017-04-26 00:35:06 +0900

    Fix docker init.sh

--- resources/docker/home/bin/init.sh
+++ resources/docker/home/bin/init.sh
@@ -4,4 +4,4 @@
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
-schematool -initSchema -dbType derby
+[ -d ~/metastore_db ] || schematool -initSchema -dbType derby
