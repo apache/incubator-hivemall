a5067b449a23b3650e65238fad41d433bdb8b6cc
Takuya Kitazawa <k.takuti@gmail.com>
2017-07-13 16:59:54 +0900

    Remove unused imports

--- nlp/src/main/java/hivemall/nlp/tokenizer/KuromojiUDF.java
+++ nlp/src/main/java/hivemall/nlp/tokenizer/KuromojiUDF.java
@@ -34,8 +34,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.ZipException;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
