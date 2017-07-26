048cda7640fdf924250e228c157647d749e4d0ef
AKHMATOVA, Elena <Elena.AKHMATOVA@suncorp.com.au>
2017-07-26 15:33:38 +1000

    headers

--- core/src/main/java/hivemall/opennlp/tools/BigGIS.java
+++ core/src/main/java/hivemall/opennlp/tools/BigGIS.java
@@ -1,3 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
 package hivemall.opennlp.tools;
 
 import java.io.IOException;
@@ -9,33 +29,31 @@ import opennlp.model.UniformPrior;
 
 public class BigGIS {
     /**
-     * Set this to false if you don't want messages about the progress of model training displayed. Alternately, you can use the overloaded version of
-     * trainModel() to conditionally enable progress messages.
+     * Set this to false if you don't want messages about the progress of
+     * model training displayed. Alternately, you can use the overloaded
+     * version of trainModel() to conditionally enable progress messages.
      */
     public static boolean PRINT_MESSAGES = false;
 
-    /**
-     * If we are using smoothing, this is used as the "number" of times we want the trainer to imagine that it saw a feature that it actually didn't
-     * see. Defaulted to 0.1.
+    /** If we are using smoothing, this is used as the "number" of
+     * times we want the trainer to imagine that it saw a feature that it
+     * actually didn't see.  Defaulted to 0.1.
      */
     public static double SMOOTHING_OBSERVATION = 0.1;
-
+    
     /**
      * Train a model using the GIS algorithm.
-     * 
      * @param iterations The number of GIS iterations to perform.
      * @param indexer The object which will be used for event compilation.
      * @param matrix Matrix with input data.
      */
-
-    public static GISModel trainModel(int iterations, BigDataIndexer indexer,
-            MatrixForTraining matrix) {
-        return trainModel(iterations, indexer, matrix, PRINT_MESSAGES, false, new UniformPrior(), 0);
+    
+    public static GISModel trainModel(int iterations, BigDataIndexer indexer, MatrixForTraining matrix) {
+    	return trainModel(iterations, indexer, matrix, PRINT_MESSAGES, false, new UniformPrior(), 0);
     }
-
+ 
     /**
      * Train a model using the GIS algorithm.
-     * 
      * @param iterations The number of GIS iterations to perform.
      * @param indexer The object which will be used for event compilation.
      * @param matrix Matrix with input data.
@@ -43,15 +61,14 @@ public class BigGIS {
      * @param smoothing Defines whether the created trainer will use smoothing while training the model.
      * @param modelPrior The prior distribution for the model.
      * @param cutoff The number of times a predicate must occur to be used in a model.
-     * @return The newly trained model, which can be used immediately or saved to disk using an opennlp.maxent.io.GISModelWriter object.
+     * @return The newly trained model, which can be used immediately or saved
+     *         to disk using an opennlp.maxent.io.GISModelWriter object.
      */
-
-    public static GISModel trainModel(int iterations, BigDataIndexer indexer,
-            MatrixForTraining matrix, boolean printMessagesWhileTraining, boolean smoothing,
-            Prior modelPrior, int cutoff) {
-        BigGISTrainer trainer = new BigGISTrainer(printMessagesWhileTraining);
+    
+    public static GISModel trainModel(int iterations, BigDataIndexer indexer, MatrixForTraining matrix, boolean printMessagesWhileTraining, boolean smoothing, Prior modelPrior, int cutoff) {
+    	BigGISTrainer trainer = new BigGISTrainer(printMessagesWhileTraining);
         trainer.setSmoothing(false);
         trainer.setSmoothingObservation(SMOOTHING_OBSERVATION);
-        return trainer.trainModel(iterations, indexer, matrix, smoothing, modelPrior, cutoff);
-    }
+    	return trainer.trainModel(iterations, indexer, matrix, smoothing, modelPrior, cutoff);
+    }  
 }
