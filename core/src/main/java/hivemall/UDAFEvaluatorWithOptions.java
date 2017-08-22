bfb6c524101b74f910d53fe17a206f7f0618d8ce
nzw0301 <k_nzw@klis.tsukuba.ac.jp>
2017-08-22 19:14:53 +0900

    Comments reflect

--- core/src/main/java/hivemall/UDAFEvaluatorWithOptions.java
+++ core/src/main/java/hivemall/UDAFEvaluatorWithOptions.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package hivemall;
 
 import hivemall.utils.lang.CommandLineUtils;
@@ -9,7 +27,7 @@ import org.apache.hadoop.hive.ql.exec.MapredContext;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.mapred.Counters;
+import org.apache.hadoop.mapred.Counters.Counter;
 import org.apache.hadoop.mapred.Reporter;
 
 import javax.annotation.Nonnull;
@@ -43,7 +61,7 @@ public abstract class UDAFEvaluatorWithOptions extends GenericUDAFEvaluator {
         }
     }
 
-    protected static void setCounterValue(@Nullable Counters.Counter counter, long value) {
+    protected static void setCounterValue(@Nullable Counter counter, long value) {
         if (counter != null) {
             synchronized (counter) {
                 counter.setValue(value);
@@ -51,7 +69,7 @@ public abstract class UDAFEvaluatorWithOptions extends GenericUDAFEvaluator {
         }
     }
 
-    protected static void incrCounter(@Nullable Counters.Counter counter, long incr) {
+    protected static void incrCounter(@Nullable Counter counter, long incr) {
         if (counter != null) {
             synchronized (counter) {
                 counter.increment(incr);
