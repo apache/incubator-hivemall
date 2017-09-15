bef36ed9dd4bcd11bd46c156d85e8b2006d3811d
Makoto Yui <myui@apache.org>
2017-09-15 10:28:23 +0900

    Fixed nDCG, MRR, AP bugs and applied refactoring

--- core/src/test/java/hivemall/evaluation/BinaryResponsesMeasuresTest.java
+++ core/src/test/java/hivemall/evaluation/BinaryResponsesMeasuresTest.java
@@ -18,8 +18,8 @@
  */
 package hivemall.evaluation;
 
-import java.util.Collections;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Assert;
@@ -40,6 +40,18 @@ public class BinaryResponsesMeasuresTest {
     }
 
     @Test
+    public void testNDCG2() {
+        List<Integer> rankedList = Arrays.asList(3, 2, 1, 6);
+        List<Integer> groundTruth = Arrays.asList(1);
+
+        double actual = BinaryResponsesMeasures.nDCG(rankedList, groundTruth, 2);
+        Assert.assertEquals(0.d, actual, 0.0001d);
+
+        actual = BinaryResponsesMeasures.nDCG(rankedList, groundTruth, 3);
+        Assert.assertEquals(0.5d, actual, 0.0001d);
+    }
+
+    @Test
     public void testRecall() {
         List<Integer> rankedList = Arrays.asList(1, 3, 2, 6);
         List<Integer> groundTruth = Arrays.asList(1, 2, 4);
@@ -49,6 +61,16 @@ public class BinaryResponsesMeasuresTest {
 
         actual = BinaryResponsesMeasures.Recall(rankedList, groundTruth, 2);
         Assert.assertEquals(0.3333333333333333d, actual, 0.0001d);
+    }
+
+    @Test
+    public void testRecallEmpty() {
+        Assert.assertEquals(1.d,
+            BinaryResponsesMeasures.Recall(Collections.emptyList(), Collections.emptyList(), 2),
+            0.d);
+
+        Assert.assertEquals(0.d,
+            BinaryResponsesMeasures.Recall(Arrays.asList(1, 3, 2), Collections.emptyList(), 2), 0.d);
     }
 
     @Test
@@ -65,32 +87,91 @@ public class BinaryResponsesMeasuresTest {
     }
 
     @Test
-    public void testMRR() {
+    public void testPrecisionEmpty() {
+        Assert.assertEquals(1.d,
+            BinaryResponsesMeasures.Precision(Collections.emptyList(), Collections.emptyList(), 2),
+            0.d);
+
+        Assert.assertEquals(0.d,
+            BinaryResponsesMeasures.Precision(Arrays.asList(1, 3, 2), Collections.emptyList(), 2),
+            0.d);
+    }
+
+    @Test
+    public void testRR() {
         List<Integer> rankedList = Arrays.asList(1, 3, 2, 6);
         List<Integer> groundTruth = Arrays.asList(1, 2, 4);
 
-        double actual = BinaryResponsesMeasures.MRR(rankedList, groundTruth, rankedList.size());
+        double actual = BinaryResponsesMeasures.ReciprocalRank(rankedList, groundTruth,
+            rankedList.size());
         Assert.assertEquals(1.0d, actual, 0.0001d);
 
         Collections.reverse(rankedList);
 
-        actual = BinaryResponsesMeasures.MRR(rankedList, groundTruth, rankedList.size());
+        actual = BinaryResponsesMeasures.ReciprocalRank(rankedList, groundTruth, rankedList.size());
         Assert.assertEquals(0.5d, actual, 0.0001d);
 
-        actual = BinaryResponsesMeasures.MRR(rankedList, groundTruth, 1);
+        actual = BinaryResponsesMeasures.ReciprocalRank(rankedList, groundTruth, 1);
         Assert.assertEquals(0.0d, actual, 0.0001d);
     }
 
     @Test
-    public void testMAP() {
+    public void testAP() {
         List<Integer> rankedList = Arrays.asList(1, 3, 2, 6);
         List<Integer> groundTruth = Arrays.asList(1, 2, 4);
 
-        double actual = BinaryResponsesMeasures.MAP(rankedList, groundTruth, rankedList.size());
-        Assert.assertEquals(0.5555555555555555d, actual, 0.0001d);
+        double actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth,
+            rankedList.size());
+        Assert.assertEquals(1.0 / 2.0 * (1.0 / 1.0 + 2.0 / 3.0), actual, 0.0001d);
 
-        actual = BinaryResponsesMeasures.MAP(rankedList, groundTruth, 2);
-        Assert.assertEquals(0.3333333333333333d, actual, 0.0001d);
+        actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 4);
+        Assert.assertEquals(1.0 / 2.0 * (1.0 / 1.0 + 2.0 / 3.0), actual, 0.0001d);
+
+        actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 3);
+        Assert.assertEquals(1.0 / 2.0 * (1.0 / 1.0 + 2.0 / 3.0), actual, 0.0001d);
+
+        actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 2);
+        Assert.assertEquals(1.0 / 1.0 * (1.0 / 1.0), actual, 0.0001d);
+
+        rankedList = Arrays.asList(3, 1, 2, 6);
+        actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 2);
+        Assert.assertEquals(1.0 / 1.0 * (1.0 / 2.0), actual, 0.0001d);
+
+        groundTruth = Arrays.asList(1, 2, 3);
+        actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 2);
+        Assert.assertEquals(1.0 / 2.0 * (1.0 / 1.0 + 2.0 / 2.0), actual, 0.0001d);
+
+        rankedList = Arrays.asList(3, 1);
+        groundTruth = Arrays.asList(1, 2);
+        actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 2);
+        Assert.assertEquals(1.0 / 1.0 * (1.0 / 2.0), actual, 0.0001d);
+    }
+
+    @Test
+    public void testAPString() {
+        List<String> rankedList = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+        List<String> groundTruth = Arrays.asList("a", "x", "x", "d", "x", "x");
+
+        double actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 6);
+        Assert.assertEquals(0.75d, actual, 0.0001d);
+    }
+
+    @Test
+    public void testAPString10() {
+        List<String> rankedList = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j");
+        List<String> groundTruth = Arrays.asList("a", "x", "c", "x", "e", "f");
+
+        double actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 10);
+        Assert.assertEquals(1.0 / 4.0 * (1.0 / 1.0 + 2.0 / 3.0 + 3.0 / 5.0 + 4.0 / 6.0), actual,
+            0.0001d);
+
+        actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 5);
+        Assert.assertEquals(1.0 / 3.0 * (1.0 / 1.0 + 2.0 / 3.0 + 3.0 / 5.0), actual, 0.0001d);
+
+        groundTruth = Arrays.asList("a", "x", "c", "x", "e", "f", "x", "x", "x", "x");
+        actual = BinaryResponsesMeasures.AveragePrecision(rankedList, groundTruth, 10);
+        Assert.assertEquals(1.0 / 4.0 * (1.0 / 1.0 + 2.0 / 3.0 + 3.0 / 5.0 + 4.0 / 6.0), actual,
+            0.0001d);
     }
 
     @Test
