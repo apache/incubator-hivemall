968f4cc9d9e5f842fe0f1cc487e8ae530ae35d9b
Makoto Yui <myui@apache.org>
2017-10-23 16:20:39 +0900

    Refactored Int2LongOpenHashTable

--- core/src/main/java/hivemall/utils/collections/maps/Int2LongOpenHashTable.java
+++ core/src/main/java/hivemall/utils/collections/maps/Int2LongOpenHashTable.java
@@ -51,7 +51,7 @@ public final class Int2LongOpenHashTable implements Externalizable {
     public static final int DEFAULT_SIZE = 65536;
     public static final float DEFAULT_LOAD_FACTOR = 0.75f;
     public static final float DEFAULT_GROW_FACTOR = 2.0f;
-
+    
     protected final transient float _loadFactor;
     protected final transient float _growFactor;
 
@@ -60,6 +60,7 @@ public final class Int2LongOpenHashTable implements Externalizable {
     protected byte[] _states;
 
     protected int _used;
+
     protected int _threshold;
     protected long defaultReturnValue = -1L;
 
@@ -79,7 +80,8 @@ public final class Int2LongOpenHashTable implements Externalizable {
         this(size, loadFactor, growFactor, true);
     }
 
-    protected Int2LongOpenHashTable(int size, float loadFactor, float growFactor, boolean forcePrime) {
+    protected Int2LongOpenHashTable(int size, float loadFactor, float growFactor,
+            boolean forcePrime) {
         if (size < 1) {
             throw new IllegalArgumentException();
         }
@@ -165,19 +167,31 @@ public final class Int2LongOpenHashTable implements Externalizable {
                 return old;
             }
             // try second hash
+            final int loopIndex = keyIdx;
             final int decr = 1 + (hash % (keyLength - 2));
             for (;;) {
                 keyIdx -= decr;
                 if (keyIdx < 0) {
                     keyIdx += keyLength;
                 }
-                if (isFree(keyIdx, key)) {
+                if (keyIdx == loopIndex) {
+                    throw new IllegalStateException(
+                        "Detected infinite loop where key=" + key + ", keyIdx=" + keyIdx);
+                }
+
+                final byte state = states[keyIdx];
+                if (state == FREE) {
                     break;
                 }
-                if (states[keyIdx] == FULL && keys[keyIdx] == key) {
-                    long old = values[keyIdx];
-                    values[keyIdx] = value;
-                    return old;
+                if (keys[keyIdx] == key) {
+                    if (state == FULL) {
+                        long old = values[keyIdx];
+                        values[keyIdx] = value;
+                        return old;
+                    } else {
+                        assert (state == REMOVED);
+                        break;
+                    }
                 }
             }
         }
@@ -186,18 +200,6 @@ public final class Int2LongOpenHashTable implements Externalizable {
         states[keyIdx] = FULL;
         ++_used;
         return defaultReturnValue;
-    }
-
-    /** Return weather the required slot is free for new entry */
-    protected boolean isFree(final int index, final int key) {
-        final byte stat = _states[index];
-        if (stat == FREE) {
-            return true;
-        }
-        if (stat == REMOVED && _keys[index] == key) {
-            return true;
-        }
-        return false;
     }
 
     /** @return expanded or not */
@@ -215,64 +217,44 @@ public final class Int2LongOpenHashTable implements Externalizable {
         final byte[] states = _states;
         final int keyLength = keys.length;
 
+        // double hashing
         final int hash = keyHash(key);
-        int keyIdx = hash % keyLength;
-        if (states[keyIdx] != FREE) {
-            if (states[keyIdx] == FULL && keys[keyIdx] == key) {
-                return keyIdx;
+        final int decr = 1 + (hash % (keyLength - 2));
+        final int startIndex = hash % keyLength;
+        for (int keyIdx = startIndex;;) {
+            final byte state = states[keyIdx];
+            if (state == FREE) {
+                return -1;
             }
-            // try second hash
-            final int decr = 1 + (hash % (keyLength - 2));
-            for (;;) {
-                keyIdx -= decr;
-                if (keyIdx < 0) {
-                    keyIdx += keyLength;
-                }
-                if (isFree(keyIdx, key)) {
+            if (keys[keyIdx] == key) {
+                if (state == FULL) {
+                    return keyIdx;
+                } else {
+                    assert (state == REMOVED);
                     return -1;
                 }
-                if (states[keyIdx] == FULL && keys[keyIdx] == key) {
-                    return keyIdx;
-                }
+            }
+            keyIdx -= decr;
+            if (keyIdx < 0) {
+                keyIdx += keyLength;
+            }
+            if (keyIdx == startIndex) {
+                throw new IllegalStateException(
+                    "Detected infinite loop where key=" + key + ", keyIdx=" + keyIdx);
             }
         }
-        return -1;
     }
 
     public long remove(final int key) {
-        final int[] keys = _keys;
-        final long[] values = _values;
-        final byte[] states = _states;
-        final int keyLength = keys.length;
-
-        final int hash = keyHash(key);
-        int keyIdx = hash % keyLength;
-        if (states[keyIdx] != FREE) {
-            if (states[keyIdx] == FULL && keys[keyIdx] == key) {
-                long old = values[keyIdx];
-                states[keyIdx] = REMOVED;
-                --_used;
-                return old;
-            }
-            //  second hash
-            final int decr = 1 + (hash % (keyLength - 2));
-            for (;;) {
-                keyIdx -= decr;
-                if (keyIdx < 0) {
-                    keyIdx += keyLength;
-                }
-                if (states[keyIdx] == FREE) {
-                    return defaultReturnValue;
-                }
-                if (states[keyIdx] == FULL && keys[keyIdx] == key) {
-                    long old = values[keyIdx];
-                    states[keyIdx] = REMOVED;
-                    --_used;
-                    return old;
-                }
-            }
+        final int keyIdx = findKey(key);
+        if (keyIdx == -1) {
+            return defaultReturnValue;
         }
-        return defaultReturnValue;
+
+        long old = _values[keyIdx];
+        _states[keyIdx] = REMOVED;
+        --_used;
+        return old;
     }
 
     public int size() {
@@ -318,27 +300,37 @@ public final class Int2LongOpenHashTable implements Externalizable {
     }
 
     private void rehash(final int newCapacity) {
-        int oldCapacity = _keys.length;
+        final int[] oldKeys = _keys;
+        final long[] oldValues = _values;
+        final byte[] oldStates = _states;
+
+        final int oldCapacity = oldKeys.length;
         if (newCapacity <= oldCapacity) {
             throw new IllegalArgumentException("new: " + newCapacity + ", old: " + oldCapacity);
         }
+
         final int[] newkeys = new int[newCapacity];
         final long[] newValues = new long[newCapacity];
         final byte[] newStates = new byte[newCapacity];
         int used = 0;
         for (int i = 0; i < oldCapacity; i++) {
-            if (_states[i] == FULL) {
+            if (oldStates[i] == FULL) {
                 used++;
-                final int k = _keys[i];
-                final long v = _values[i];
+                final int k = oldKeys[i];
+                final long v = oldValues[i];
                 final int hash = keyHash(k);
                 int keyIdx = hash % newCapacity;
                 if (newStates[keyIdx] == FULL) {// second hashing
                     final int decr = 1 + (hash % (newCapacity - 2));
+                    final int loopIndex = keyIdx;
                     while (newStates[keyIdx] != FREE) {
                         keyIdx -= decr;
                         if (keyIdx < 0) {
                             keyIdx += newCapacity;
+                        }
+                        if (keyIdx == loopIndex) {
+                            throw new IllegalStateException(
+                                "Detected infinite loop where key=" + k + ", keyIdx=" + keyIdx);
                         }
                     }
                 }
