e4ed6966c8639fd526f13df1d8f4f4b9eb76138d
Makoto Yui <myui@apache.org>
2017-11-06 15:14:32 +0900

    Update the scheme to udpate version string

--- bin/set_version.sh
+++ bin/set_version.sh
@@ -31,7 +31,7 @@ fi
 
 cd $HIVEMALL_HOME
 
-yes_or_no () {
+function yes_or_no() {
   while true; do
     echo "Type [Y/N]"
     echo -n ">>"
@@ -48,23 +48,35 @@ yes_or_no () {
   done
 }
 
+old_version=`cat VERSION`
+echo "Current version number is ${old_version}"
+echo
+
 echo "This script will update the version string of Hivemall."
 echo
 echo "Please input a version string (e.g., 0.4.3-rc.2)"
 echo -n ">>"
-read version
+read new_version
 
-echo "Do you really want to update Hivemall version string to ${version}?"
+echo
+echo "--------------------------------------------------------------------------"
+echo "[Here are the list of files to update]"
+echo
+find . -type f \( -name 'VERSION' -o -name 'pom.xml' -o -name 'HivemallConstants.java' -o -name 'HivemallOpsSuite.scala' -o -name 'HiveUdfSuite.scala' \)  | xargs grep ${old_version}
+echo "--------------------------------------------------------------------------"
+echo
+
+echo "Do you really want to update Hivemall version string from ${old_version} to ${new_version}?"
+echo
+
 yes_or_no
 
 if [ "$?" -eq 1 ]; then
   echo "aborted!"
   exit 1
 fi
+echo
 
-echo "Updating ..."
-
-# Note: grep is workaround for a bug in mvn versions plugin
-# https://github.com/mojohaus/versions-maven-plugin/issues/143 
-mvn versions:set -DnewVersion=${version} -DgenerateBackupPoms=false | grep '[^expression: scala.binary.version no value]'
-mvn com.google.code.maven-replacer-plugin:replacer:replace
+echo -n "Updating ..."
+find . -type f \( -name 'VERSION' -o -name 'pom.xml' -o -name 'HivemallConstants.java' -o -name 'HivemallOpsSuite.scala' -o -name 'HiveUdfSuite.scala' \) | xargs sed -i '' -e "s/${old_version}/${new_version}/g"
+echo "Done!"
