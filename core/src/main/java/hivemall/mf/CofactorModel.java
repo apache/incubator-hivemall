270f4b6d20bf44fa1e6c90a8060dfd5a2431f01d
Jackson Huang <jackson_huang@live.com>
2018-10-15 18:09:28 +0900

    implemented updateTheta logic

--- core/src/main/java/hivemall/mf/CofactorModel.java
+++ core/src/main/java/hivemall/mf/CofactorModel.java
@@ -19,24 +19,18 @@
 package hivemall.mf;
 
 import hivemall.fm.Feature;
-import hivemall.math.matrix.sparse.DoKMatrix;
 import hivemall.utils.math.MathUtils;
-import it.unimi.dsi.fastutil.Hash;
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import hivemall.utils.math.MatrixUtils;
 import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.SingularValueDecomposition;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 public class CofactorModel {
 
@@ -87,6 +81,8 @@ public class CofactorModel {
     private Map<String, RealVector> gamma;
     private Map<String, Double> gammaBias;
 
+    // precomputed identity matrix
+    private RealMatrix identity;
 
     protected final Random[] randU, randI;
 
@@ -143,6 +139,11 @@ public class CofactorModel {
             weights.put(key, v);
         }
         return v;
+    }
+
+    private void setFactorVector(Feature f, Map<String, RealVector> weights, RealVector factorVector) {
+        assert weights.containsKey(f);
+        weights.put(f.getFeature(), factorVector);
     }
 
     private double getBias(Feature f, Map<String, Double> biases) {
@@ -221,7 +222,111 @@ public class CofactorModel {
      * Update latent factors of the users in the provided mini-batch.
      */
     public void updateTheta(List<CofactorizationUDTF.TrainingSample> samples) {
+        // initialize item factors
+        // items should only be trainable if the dataset contains a major entry for that item (which it may not)
 
+        // variable names follow cofacto.py
+        RealMatrix BTB = precomputeBetaTBeta();
+        if (identity == null) {
+            identity = new Array2DRowRealMatrix(MatrixUtils.eye(factor));
+            identity = identity.scalarMultiply(lambdaTheta);
+        }
+
+        RealMatrix BTBpR = BTB.add(identity);
+
+        String itemName;
+
+        for (CofactorizationUDTF.TrainingSample sample : samples) {
+            // filter for trainable items
+            List<Feature> trainableItems = new ArrayList<>();
+            for (Feature item : sample.children) {
+                itemName = item.getFeature();
+                if (isTrainable(itemName, true)) {
+                    trainableItems.add(item);
+                }
+            }
+
+            RealMatrix A = calculateA(trainableItems);
+
+            RealMatrix delta = calculateDelta(trainableItems);
+            RealMatrix B = BTBpR.add(delta);
+
+            // solve and update factors
+            RealMatrix newThetaMatrix = solve(B, A);
+            setFactorVector(sample.parent, theta, newThetaMatrix.getRowVector(0));
+        }
+    }
+
+    private RealMatrix solve(RealMatrix b, RealMatrix a) {
+        // b * x = a
+        // solves for x
+        SingularValueDecomposition svd = new SingularValueDecomposition(b);
+        return svd.getSolver().solve(a);
+
+    }
+
+    private RealMatrix calculateDelta(List<Feature> items) {
+        RealMatrix delta = new Array2DRowRealMatrix(factor, factor);
+        int i = 0, j = 0;
+        for (int f = 0; f < factor; f++) {
+            for (int ff = 0; ff < factor; ff++) {
+                double val = (c1 - c0) * dotItemFactorsAlongDims(items, f, ff);
+                delta.setEntry(f, ff, val);
+            }
+        }
+        return delta;
+    }
+
+    private double dotItemFactorsAlongDims(List<Feature> items, int dim1, int dim2) {
+        double result = 0.d;
+        for (Feature item : items) {
+            RealVector vec = getBetaVector(item);
+            result += vec.getEntry(dim1) * vec.getEntry(dim2);
+        }
+        return result;
+    }
+
+    private RealMatrix calculateA(List<Feature> items) {
+        // Equivalent to: a = x_u.dot(c1 * B_u)
+        // x_u is a (1, i) matrix of all ones
+        // B_u is a (i, F) matrix
+        // What it does: sums factor n of each item in B_u
+        RealVector v = new ArrayRealVector(items.size());
+        for (Feature item : items) {
+            v.add(getBetaVector(item));
+        }
+        for (int a = 0; a < v.getDimension(); a++) {
+            v.setEntry(a, v.getEntry(a) / c1);
+        }
+        // convert RealVector to 1-row RealMatrix
+        RealMatrix A = new Array2DRowRealMatrix(1, items.size());
+        A.setRowVector(0, v);
+        return A;
+    }
+
+    private RealMatrix precomputeBetaTBeta() {
+        RealMatrix BTB = new Array2DRowRealMatrix(factor, factor);
+        for (RealVector u : beta.values()) {
+            for (RealVector v : beta.values()) {
+                accumulateInBetaTBeta(u, v, BTB);
+            }
+        }
+        return BTB;
+    }
+
+
+    private void accumulateInBetaTBeta(RealVector u, RealVector v, RealMatrix BTB) {
+        for (int i = 0; i < u.getDimension(); i++) {
+            double val = u.getEntry(i);
+            for (int j = 0; j < v.getDimension(); j++) {
+                double newEntry = BTB.getEntry(i, j) + c0 * val * v.getEntry(j);
+                BTB.setEntry(i, j, newEntry);
+            }
+        }
+    }
+
+    private boolean isTrainable(String name, boolean isItem) {
+        return isItem ? beta.containsKey(name) : theta.containsKey(name);
     }
 
     @Nonnull
