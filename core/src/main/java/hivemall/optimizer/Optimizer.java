b8e5698ecd7e7d2758ef85a338c053f5bbcc663d
Makoto Yui <myui@apache.org>
2018-12-07 18:13:48 +0900

    Supported -amsgrad in Adam

--- core/src/main/java/hivemall/optimizer/Optimizer.java
+++ core/src/main/java/hivemall/optimizer/Optimizer.java
@@ -211,11 +211,15 @@ public interface Optimizer {
         private final float beta1, beta2;
         private final float eps;
 
+        private final boolean amsgrad;
+        private float max_vhat = Float.MIN_VALUE;
+
         public Adam(@Nonnull Map<String, String> options) {
             super(options);
             this.beta1 = Primitives.parseFloat(options.get("beta1"), 0.9f);
             this.beta2 = Primitives.parseFloat(options.get("beta2"), 0.999f);
             this.eps = Primitives.parseFloat(options.get("eps"), 1e-8f);
+            this.amsgrad = options.containsKey("amsgrad");
         }
 
         @Override
@@ -249,6 +253,13 @@ public interface Optimizer {
             float m_hat = m / (float) (1.f - Math.pow(beta1, _numStep));
             // compute bias-corrected second raw moment estimat
             float v_hat = v / (float) (1.f - Math.pow(beta2, _numStep));
+            if (amsgrad) {
+                if (v_hat > max_vhat) {
+                    this.max_vhat = v_hat;
+                } else {// v_hat <= max_vhat
+                    v_hat = max_vhat;
+                }
+            }
             // compute delta update
             float delta = m_hat / (float) (Math.sqrt(v_hat) + eps);
             weight.setM(m);
