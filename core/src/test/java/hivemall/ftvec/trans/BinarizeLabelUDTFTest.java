0c7ba1734006a69b3173758b7a7106a835b3c6c2
Makoto Yui <myui@apache.org>
2019-10-21 16:22:05 +0900

    [HIVEMALL-244] Support Java9, Java11(LTS)
    ## What changes were proposed in this pull request?
    
    Support Java9, Java11(LTS)
    
    ## What type of PR is it?
    
    Improvement | Hot Fix
    
    ## What is the Jira issue?
    
    https://issues.apache.org/jira/browse/HIVEMALL-244
    
    ## How was this patch tested?
    
    unit tests
    
    ## Checklist
    
    (Please remove this section if not needed; check `x` for YES, blank for NO)
    
    - [x] Did you apply source code formatter, i.e., `./bin/format_code.sh`, for your commit?
    - [ ] Did you run system tests on Hive (or Spark)?
    
    Author: Makoto Yui <myui@apache.org>
    
    Closes #203 from myui/HIVEMALL-244.

--- core/src/test/java/hivemall/ftvec/trans/BinarizeLabelUDTFTest.java
+++ core/src/test/java/hivemall/ftvec/trans/BinarizeLabelUDTFTest.java
@@ -18,13 +18,6 @@
  */
 package hivemall.ftvec.trans;
 
-import static org.mockito.AdditionalMatchers.aryEq;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.powermock.api.mockito.PowerMockito.doNothing;
-import static org.powermock.api.mockito.PowerMockito.spy;
-import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
-
 import hivemall.TestUtils;
 import hivemall.utils.hadoop.WritableUtils;
 
@@ -36,12 +29,7 @@ import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({BinarizeLabelUDTF.class})
 public class BinarizeLabelUDTFTest {
 
     // ignored to avoid
@@ -57,54 +45,6 @@ public class BinarizeLabelUDTFTest {
             PrimitiveObjectInspectorFactory.javaStringObjectInspector, featureNames);
 
         udtf.initialize(argOIs);
-    }
-
-    @Test
-    public void test2Positive3NegativeSample() throws Exception {
-        BinarizeLabelUDTF udtf = spy(new BinarizeLabelUDTF());
-        ObjectInspector[] argOIs = new ObjectInspector[3];
-        argOIs[0] = PrimitiveObjectInspectorFactory.javaIntObjectInspector;
-        argOIs[1] = PrimitiveObjectInspectorFactory.javaIntObjectInspector;
-        List<String> featureNames = Arrays.asList("positive", "negative", "features");
-        argOIs[2] = ObjectInspectorFactory.getStandardConstantListObjectInspector(
-            PrimitiveObjectInspectorFactory.javaStringObjectInspector, featureNames);
-
-        doNothing().when(udtf, "forward", any());
-        udtf.initialize(argOIs);
-
-        Object[] arguments = new Object[3];
-        arguments[0] = new Integer(2);
-        arguments[1] = new Integer(3);
-        arguments[2] = WritableUtils.val("a:1", "b:2");
-        udtf.process(arguments);
-
-        verifyPrivate(udtf, times(5)).invoke("forward", any(Object[].class));
-        verifyPrivate(udtf, times(2)).invoke("forward",
-            aryEq(new Object[] {WritableUtils.val("a:1", "b:2"), 1}));
-        verifyPrivate(udtf, times(3)).invoke("forward",
-            aryEq(new Object[] {WritableUtils.val("a:1", "b:2"), 0}));
-    }
-
-    @Test
-    public void test0Positive0NegativeSample() throws Exception {
-        BinarizeLabelUDTF udtf = spy(new BinarizeLabelUDTF());
-        ObjectInspector[] argOIs = new ObjectInspector[3];
-        argOIs[0] = PrimitiveObjectInspectorFactory.javaIntObjectInspector;
-        argOIs[1] = PrimitiveObjectInspectorFactory.javaIntObjectInspector;
-        List<String> featureNames = Arrays.asList("positive", "negative", "features");
-        argOIs[2] = ObjectInspectorFactory.getStandardConstantListObjectInspector(
-            PrimitiveObjectInspectorFactory.javaStringObjectInspector, featureNames);
-
-        doNothing().when(udtf, "forward", any());
-        udtf.initialize(argOIs);
-
-        Object[] arguments = new Object[3];
-        arguments[0] = new Integer(0);
-        arguments[1] = new Integer(0);
-        arguments[2] = WritableUtils.val("a:1", "b:2");
-        udtf.process(arguments);
-
-        verifyPrivate(udtf, times(0)).invoke("forward", any(Object[].class));
     }
 
     @Test
