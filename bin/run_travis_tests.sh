922880332e99db3857249145c9b8bf0843fe154c
Makoto Yui <myui@apache.org>
2019-10-21 13:57:48 +0900

    Fix for CI errors

--- bin/run_travis_tests.sh
+++ bin/run_travis_tests.sh
@@ -18,17 +18,6 @@
 # under the License.
 #
 
-if [ "$HIVEMALL_HOME" = "" ]; then
-  if [ -e ../bin/${0##*/} ]; then
-    HIVEMALL_HOME=".."
-  elif [ -e ./bin/${0##*/} ]; then
-    HIVEMALL_HOME="."
-  else
-    echo "env HIVEMALL_HOME not defined"
-    exit 1
-  fi
-fi
-
 set -ev
 
 mvn clean test
