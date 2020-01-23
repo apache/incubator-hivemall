527cd9779ff823bd754af2fc775935cc1bcd5f89
Makoto Yui <myui@apache.org>
2020-01-23 19:20:02 +0900

    Updated function doc

--- core/src/main/java/hivemall/tools/list/UDAFToOrderedList.java
+++ core/src/main/java/hivemall/tools/list/UDAFToOrderedList.java
@@ -72,27 +72,41 @@ import org.apache.hadoop.io.IntWritable;
 @Description(name = "to_ordered_list",
         value = "_FUNC_(PRIMITIVE value [, PRIMITIVE key, const string options])"
                 + " - Return list of values sorted by value itself or specific key",
-        extended = "WITH t as (\n" + "    SELECT 5 as key, 'apple' as value\n" + "    UNION ALL\n"
-                + "    SELECT 3 as key, 'banana' as value\n" + "    UNION ALL\n"
-                + "    SELECT 4 as key, 'candy' as value\n" + "    UNION ALL\n"
-                + "    SELECT 2 as key, 'donut' as value\n" + "    UNION ALL\n"
-                + "    SELECT 3 as key, 'egg' as value\n" + ")\n"
-                + "SELECT                                             -- expected output\n"
-                + "    to_ordered_list(value, key, '-reverse'),       -- [apple, candy, (banana, egg | egg, banana), donut] (reverse order)\n"
-                + "    to_ordered_list(value, key, '-k 2'),           -- [apple, candy] (top-k)\n"
-                + "    to_ordered_list(value, key, '-k 100'),         -- [apple, candy, (banana, egg | egg, banana), dunut]\n"
-                + "    to_ordered_list(value, key, '-k 2 -reverse'),  -- [donut, (banana | egg)] (reverse top-k = tail-k)\n"
-                + "    to_ordered_list(value, key),                   -- [donut, (banana, egg | egg, banana), candy, apple] (natural order)\n"
-                + "    to_ordered_list(value, key, '-k -2'),          -- [donut, (banana | egg)] (tail-k)\n"
-                + "    to_ordered_list(value, key, '-k -100'),        -- [donut, (banana, egg | egg, banana), candy, apple]\n"
-                + "    to_ordered_list(value, key, '-k -2 -reverse'), -- [apple, candy] (reverse tail-k = top-k)\n"
-                + "    to_ordered_list(value, '-k 2'),                -- [egg, donut] (alphabetically)\n"
-                + "    to_ordered_list(key, '-k -2 -reverse'),        -- [5, 4] (top-2 keys)\n"
-                + "    to_ordered_list(key),                          -- [2, 3, 3, 4, 5] (natural ordered keys)\n"
-                + "    to_ordered_list(value, key, '-k 2 -kv_map'),   -- {4:\"candy\",5:\"apple\"}\n"
-                + "    to_ordered_list(value, key, '-k 2 -vk_map')    -- {\"candy\":4,\"apple\":5}\n"
-                + "    to_ordered_list(value, key, '-dedup -vk_map')  -- "
-                + "FROM\n" + "    t")
+        extended = "WITH data as (\n" + 
+                "    SELECT 5 as key, 'apple' as value\n" + 
+                "    UNION ALL\n" + 
+                "    SELECT 3 as key, 'banana' as value\n" + 
+                "    UNION ALL\n" + 
+                "    SELECT 4 as key, 'candy' as value\n" + 
+                "    UNION ALL\n" + 
+                "    SELECT 1 as key, 'donut' as value\n" + 
+                "    UNION ALL\n" + 
+                "    SELECT 2 as key, 'egg' as value \n" + 
+                "    UNION ALL\n" + 
+                "    SELECT 4 as key, 'candy' as value -- both key and value duplicates\n" + 
+                ")\n" + 
+                "SELECT                                                  -- expected output\n" + 
+                "    to_ordered_list(value, key, '-reverse'),            -- [apple, candy, (banana, egg | egg, banana), donut] (reverse order)\n" + 
+                "    to_ordered_list(value, key, '-k 2'),                -- [apple, candy] (top-k)\n" + 
+                "    to_ordered_list(value, key, '-k 100'),              -- [apple, candy, (banana, egg | egg, banana), dunut]\n" + 
+                "    to_ordered_list(value, key, '-k 2 -reverse'),       -- [donut, (banana | egg)] (reverse top-k = tail-k)\n" + 
+                "    to_ordered_list(value, key),                        -- [donut, (banana, egg | egg, banana), candy, apple] (natural order)\n" + 
+                "    to_ordered_list(value, key, '-k -2'),               -- [donut, (banana | egg)] (tail-k)\n" + 
+                "    to_ordered_list(value, key, '-k -100'),             -- [donut, (banana, egg | egg, banana), candy, apple]\n" + 
+                "    to_ordered_list(value, key, '-k -2 -reverse'),      -- [apple, candy] (reverse tail-k = top-k)\n" + 
+                "    to_ordered_list(value, '-k 2'),                     -- [egg, donut] (alphabetically)\n" + 
+                "    to_ordered_list(key, '-k -2 -reverse'),             -- [5, 4] (top-2 keys)\n" + 
+                "    to_ordered_list(key),                               -- [2, 3, 3, 4, 5] (natural ordered keys)\n" + 
+                "    to_ordered_list(value, key, '-k 2 -kv_map'),        -- {5:\"apple\",4:\"candy\"}\n" + 
+                "    to_ordered_list(value, key, '-k 2 -vk_map'),        -- {\"apple\":5,\"candy\":4}\n" + 
+                "    to_ordered_list(value, key, '-k -2 -kv_map'),       -- {1:\"donut\",2:\"egg\"}\n" + 
+                "    to_ordered_list(value, key, '-k -2 -vk_map'),       -- {\"donut\":1,\"egg\":2}\n" + 
+                "    to_ordered_list(value, key, '-k 4 -dedup -vk_map'), -- {\"apple\":5,\"candy\":4,\"banana\":3,\"egg\":2}\n" + 
+                "    to_ordered_list(value, key, '-k 4 -vk_map'),        -- {\"apple\":5,\"candy\":4,\"banana\":3}\n" + 
+                "    to_ordered_list(value, key, '-k 4 -dedup'),         -- [\"apple\",\"candy\",\"banana\",\"egg\"]\n" + 
+                "    to_ordered_list(value, key, '-k 4')                 -- [\"apple\",\"candy\",\"candy\",\"banana\"]\n" + 
+                "FROM\n" + 
+                "    data")
 //@formatter:on
 public final class UDAFToOrderedList extends AbstractGenericUDAFResolver {
 
