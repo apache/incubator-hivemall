1a62e7645a7b80bd16a15a0e3e567b08940ad2be
Makoto Yui <myui@apache.org>
2021-03-25 17:42:55 +0900

    Replaced tfidf macro with UDF

---
+++ core/src/main/java/hivemall/ftvec/text/TfIdfUDF.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package hivemall.ftvec.text;
+
+import hivemall.utils.hadoop.HiveUtils;
+import hivemall.utils.lang.StringUtils;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.UDFType;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.io.DoubleWritable;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.DoubleObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.LongObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils;
+
+@Description(name = "tfidf",
+        value = "_FUNC_(double termFrequency, long numDocs, const long totalNumDocs) "
+                + "- Return an TFIDF score in double.")
+@UDFType(deterministic = true, stateful = false)
+public final class TfIdfUDF extends GenericUDF {
+
+    private DoubleObjectInspector tfOI;
+    private LongObjectInspector numDocsOI;
+    private LongObjectInspector totalNumDocsOI;
+
+    @Nonnull
+    private final DoubleWritable result = new DoubleWritable();
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] argOIs) throws UDFArgumentException {
+        if (argOIs.length != 3) {
+            throw new UDFArgumentLengthException(
+                "tfidf takes exactly three arguments but got " + argOIs.length);
+        }
+
+        this.tfOI = HiveUtils.asDoubleOI(argOIs[0]);
+        this.numDocsOI = HiveUtils.asLongOI(argOIs[1]);
+        this.totalNumDocsOI = HiveUtils.asLongOI(argOIs[2]);
+
+        return PrimitiveObjectInspectorFactory.writableDoubleObjectInspector;
+    }
+
+
+    @Override
+    public Object evaluate(DeferredObject[] arguments) throws HiveException {
+        Object arg0 = getObject(arguments, 0);
+        Object arg1 = getObject(arguments, 1);
+        Object arg2 = getObject(arguments, 2);
+
+        double tf = PrimitiveObjectInspectorUtils.getDouble(arg0, tfOI);
+        long numDocs = PrimitiveObjectInspectorUtils.getLong(arg1, numDocsOI);
+        long totalNumDocs = PrimitiveObjectInspectorUtils.getLong(arg2, totalNumDocsOI);
+
+        // basic IDF
+        //    idf = log(N/n_t)
+        // IDF with smoothing
+        //    idf = log(N/(1+n_t))+1
+        //    idf = log(N/max(1,n_t))+1 -- about zero division        
+        double idf = (Math.log10(totalNumDocs / Math.max(1L, numDocs)) + 1.0d);
+        double tfidf = tf * idf;
+        result.set(tfidf);
+        return result;
+    }
+
+    @Nonnull
+    protected static Object getObject(@Nonnull final DeferredObject[] arguments,
+            @Nonnegative final int index) throws HiveException {
+        Object obj = arguments[index].get();
+        if (obj == null) {
+            throw new UDFArgumentException(String.format("%d-th argument MUST not be null", index));
+        }
+        return obj;
+    }
+
+    @Override
+    public String getDisplayString(String[] children) {
+        return "tfidf(" + StringUtils.join(children, ',') + ")";
+    }
+
+}
